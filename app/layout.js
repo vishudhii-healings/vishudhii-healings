4716cfe3b812bfc16c630cb991f106b138f3b8a2
vishudhii-healings <vishudhiihealings@gmail.com>
2026-03-29 16:13:40 +0530

    layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Vishudhii Healings',
+  description: 'Healing, Wellness & Spirituality',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
